96d56c2e039c5ee7bfdbdce9008edb3d6e4e0232
Antoine Boulinguez <antoine.boulinguez@orange.fr>
2021-02-19 16:45:42 +0100

    modif des test json avec ajout de fichiers json spécifiques aux tests

--- src/test/java/MMLJsonTest.java
+++ src/test/java/MMLJsonTest.java
@@ -40,6 +40,36 @@ public class MMLJsonTest {
 		
 	}
 	
+	@Test
+	public void testJson3() throws Exception {
+		String str = Files.readString(Paths.get("mml_test3.json")); //le json a une taille d'entraînement supérieure à 1
+		JSONObject obj = new JSONObject(str);
+		JsonTest test = new JsonTest(obj);
+		try {
+			test.trainSize();
+			fail("train_size est bien entre 0 et 1");
+		}
+		catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+		
+	}
+	
+	@Test
+	public void testJson4() throws Exception {
+		String str = Files.readString(Paths.get("mml_test4.json")); //le json désigne un langage non supporté
+		JSONObject obj = new JSONObject(str);
+		JsonTest test = new JsonTest(obj);
+		try {
+			test.language();
+			fail("Le langage est bien supporté");
+		}
+		catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+		
+	}
+	
 	
 	
 	
